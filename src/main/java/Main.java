7619de665fe212e68557f8ff8de137a92fa7ea11
Ana clara Barbosa <169068132+Ana0clara@users.noreply.github.com>
2024-05-13 14:51:44 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("—————————————");
+    System.out.println("Curso Tecnico"); 
+    System.out.println("—————————————");
+    System.out.println(" "); 
+
+
+    System.out.println("+ Titulo +"); 
+
+    System.out.println(" "); 
+    System.out.println("Texto sobre o curso e papapa "); 
+    System.out.println(" "); 
+
+    System.out.println("___________________________"); 
+
+  }
+
+  
+}
